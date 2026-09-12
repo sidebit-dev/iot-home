48f3e0bffd175b11bf343ea98aae2fc0f1439204
Miguel Arcanjo <miguel.arcanjo@outlook.com>
2026-09-12 20:01:05 -0300

    instala e configura conexao  @nestjs/typeorm typeorm pg

--- backend/src/app/app.module.ts
+++ backend/src/app/app.module.ts
@@ -2,9 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from '../user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      database: 'postgres',
+      password: '123456',
+      autoLoadEntities: true, // Carrega entidades sem precisar especifica-las
+      synchronize: true, // Sincroniza com o DB. Não deve usar em produção
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
