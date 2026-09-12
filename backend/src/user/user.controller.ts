d69c56ca5dcd94a382c4280523adaf44350287ab
Miguel Arcanjo <miguel.arcanjo@outlook.com>
2026-09-12 14:34:59 -0300

    Cria entity User e simulação de dados

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -12,34 +12,33 @@ import {
   Put,
   Query,
 } from '@nestjs/common';
+import { UserService } from './user.service';
 
 @Controller('users')
 export class UserController {
+  constructor(private readonly userService: UserService) {}
+
   @HttpCode(HttpStatus.OK)
   @Get()
   async findAll(@Query() pagination: any) {
     const { limit = 10, offset = 0 } = pagination;
     console.log(pagination);
-    return `Lista todos usuários. Limit=${limit}, offset=${offset}`;
+    return this.userService.findAll();
   }
 
   @Get(':id')
   async findOne(@Param('id', ParseIntPipe) id: number) {
-    return { user: {}, id };
+    return this.userService.findOne(id);
   }
 
   @Post()
   async create(@Body() body: any) {
-    return { body };
+    return this.userService.create(body);
   }
 
   @Put(':id')
-  async update(@Body() body: any, @Param('id', ParseIntPipe) id: number) {
-    return {
-      method: 'put',
-      body,
-      id,
-    };
+  async updateAll(@Body() body: any, @Param('id', ParseIntPipe) id: number) {
+    return this.userService.updateAll(id, body);
   }
 
   @Patch(':id')
@@ -47,15 +46,11 @@ export class UserController {
     @Body() body: any,
     @Param('id', ParseIntPipe) id: number,
   ) {
-    return {
-      method: 'patch',
-      body,
-      id,
-    };
+    return this.userService.updatePartial(id, body);
   }
 
   @Delete(':id')
   async delete(@Param('id', ParseIntPipe) id: number) {
-    return { id };
+    return this.userService.delete(id);
   }
 }
--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -1,4 +1,65 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { User } from './entities/User';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  private lastId = 1;
+  private users: User[] = [
+    {
+      id: 1,
+      name: 'Migue Ramos',
+      email: 'miguel@gmail.com',
+      password: '123456',
+      role: 'ADMIN',
+      active: true,
+      instant: new Date(),
+    },
+  ];
+  async findAll() {
+    return this.users;
+  }
+
+  async findOne(id: number) {
+    const user = this.users.find((item) => item.id === id);
+
+    if (user) return user;
+
+    throw new HttpException('Usuário não encontrado...', HttpStatus.NOT_FOUND);
+  }
+
+  async create(body: any) {
+    this.lastId++;
+    const id = this.lastId;
+    const newUser = {
+      id,
+      ...body,
+    };
+    this.users.push(newUser);
+    return newUser;
+  }
+
+  async updateAll(data: any, id: number) {
+    return { data };
+  }
+
+  async updatePartial(body: any, id: number) {
+    const userExistenteIndex = this.users.findIndex((item) => item.id === id);
+    if (userExistenteIndex >= 0) {
+      const userExistente = this.users[userExistenteIndex];
+
+      this.users[userExistenteIndex] = {
+        ...userExistente,
+        ...body,
+      };
+      return this.users[userExistenteIndex];
+    }
+  }
+
+  async delete(id: number) {
+    const userExistIndex = this.users.findIndex((item) => item.id === id);
+    if (userExistIndex >= 0) {
+      this.users.splice(userExistIndex, 1);
+    }
+    return { id };
+  }
+}
