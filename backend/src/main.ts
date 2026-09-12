c69211da0414b5d25bbd3c49d1198cb8205309fc
Miguel Arcanjo <miguel.arcanjo@outlook.com>
2026-09-12 19:24:03 -0300

    Aplica forbidNonWhitelisted no main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,7 +6,12 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // Remove chaves que não estão no DTO
+      forbidNonWhitelisted: true, // Levantar erro qdo a chave não existir
+    }),
+  );
 
   await app.listen(process.env.PORT ?? 3333);
 }
