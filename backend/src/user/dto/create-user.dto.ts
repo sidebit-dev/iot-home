54505163fa832560204490c7df129cdabd06efb6
Miguel Arcanjo <miguel.arcanjo@outlook.com>
2026-09-12 18:55:43 -0300

    Instala ValidationPipe e aplicado no Dto User

--- backend/src/user/dto/create-user.dto.ts
+++ backend/src/user/dto/create-user.dto.ts
@@ -1,5 +1,16 @@
+import { IsEmail, IsString, IsStrongPassword } from 'class-validator';
+
 export class CreateUserDto {
+  @IsString()
   readonly name: string;
+  @IsEmail()
   readonly email: string;
+  @IsStrongPassword({
+    minLength: 6,
+    minNumbers: 0,
+    minLowercase: 0,
+    minUppercase: 0,
+    minSymbols: 0,
+  })
   readonly password: string;
 }
